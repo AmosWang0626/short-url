827bcde41db795c6503c9eda476f2b4ebb7ccb33
amos.wang <1833063210@qq.com>
2020-11-22 18:13:54 +0800

    init project

---
+++ src/main/java/com/amos/shorturl/ShortUrlApplication.java
@@ -0,0 +1,16 @@
+package com.amos.shorturl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author amos.wang
+ */
+@SpringBootApplication
+public class ShortUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortUrlApplication.class, args);
+    }
+
+}
