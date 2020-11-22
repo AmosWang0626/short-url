5b7b7d1252a30607f61e9ce0adf2240e46ce3678
amos.wang <1833063210@qq.com>
2020-11-22 22:22:39 +0800

    feat: 增加短链接新增页面、404页面

--- src/main/java/com/amos/shorturl/web/ShortUrlController.java
+++ src/main/java/com/amos/shorturl/web/ShortUrlController.java
@@ -18,21 +18,44 @@ public class ShortUrlController {
 
     private static final Map<String, String> MAP = new ConcurrentHashMap<>();
 
-    private static final String BASE_URL = "localhost:8080/short/url/";
+    private static final String BASE_URL = "http://localhost:8080/short/url/";
 
     static {
         MAP.put("0", "https://amos.wang");
     }
 
+
+    /**
+     * 短链接添加页面
+     */
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+
+    /**
+     * 新增短链接
+     */
     @PostMapping
     @ResponseBody
     public String add(String url) {
+        for (Map.Entry<String, String> entry : MAP.entrySet()) {
+            if (entry.getValue().equals(url)) {
+                return BASE_URL + entry.getKey();
+            }
+        }
+
         String key = String.valueOf(MAP.size());
         MAP.put(key, url);
 
         return BASE_URL + key;
     }
 
+
+    /**
+     * 短链接访问
+     */
     @GetMapping("{key}")
     public String get(@PathVariable("key") String key) {
         String url = MAP.get(key);
@@ -40,7 +63,19 @@ public class ShortUrlController {
             return "404";
         }
 
+        // FIXME 此处需要兼容 URL 中含有中文的情况
         return "redirect:" + url;
+    }
+
+
+    /**
+     * 查看所有短链接
+     */
+    @GetMapping("all")
+    @ResponseBody
+    public Map<String, String> all() {
+
+        return MAP;
     }
 
 }
